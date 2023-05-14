80a0de2353120f63ea5d49697eeb7d6d7df30956
HayCESAR <118538535+HayCESAR@users.noreply.github.com>
2023-05-14 15:28:47 +0000

    GOOGLE_PROJECT_ID update

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-GOOGLE_PROJECT_ID=<Enter your Google project ID here>
+GOOGLE_PROJECT_ID="staiproject"
 
 gcloud beta run deploy my-api --source . \
   --platform managed \
